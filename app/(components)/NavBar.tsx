da215a815b274dac90080a23b6a007a9575b338e
Simoncl20 <simoncalderonl2010@gmail.com>
2025-03-25 23:36:22 -0500

    Add NavBar component and update layout with new design

---
+++ app/(components)/NavBar.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between py-2 font-bold px-50">
+      <Link href="/">
+      <Image src="/rick_morty_logo.svg" width={80} height={50} alt="Rick &Morty Logo"/>
+      </Link>
+      <div className="flex items-center gap-6 ">
+        <Link href="/" className="transition-colors duration-300 hover:text-green-400 hover:scale-105">
+          Characters
+        </Link>
+        <Link href="/locations" className="transition-colors duration-300 hover:text-green-400 hover:scale-105">
+          Locations
+        </Link>
+        <Link href="/episodes" className="transition-colors duration-300 hover:text-green-400 hover:scale-105">
+          Episodes
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar

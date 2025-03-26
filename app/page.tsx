8baca5f993c7211020abfe46f41f8d666f7c2ba5
Simoncl20 <simoncalderonl2010@gmail.com>
2025-03-26 04:04:12 -0500

    Add Filter component and integrate it into the Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 import Image from "next/image";
+import Search from "./(components)/Search";
+import { getCharacters } from "./lib/data";
+import { CharacterQuery } from "./lib/definitions";
+import Filter from "./(components)/Filter";
 
+ 
+export default async function Home(props : {
+  searchParams?: Promise<{query?: string, page?: string}>
+}) {
 
-export default function Home() {
+  const searchParams = await props.searchParams
+  const name = searchParams?.query || ''
+  const currentPage = (searchParams?.page) || "1"
+
+  const query : CharacterQuery = {name: name}
+
+  const charactersData = await getCharacters(query, currentPage)
+  // Log safely - only log specific properties or use JSON.stringify for safe parts
+  console.log('Characters count:', charactersData?.info?.count);
+  console.log('First character:', charactersData?.results?.[0]?.name);
 
   return (
-    <main className="flex justify-center mx-24 mt-10">
+    <main className="flex flex-col items-center mx-24 mt-10">
       <Image src="/logotype.svg" width={600} height={300} alt="Rick &Morty Logo"/>
+      <Search placeholder="Search Character"/>
+      <Filter />
     </main>
   );
 }

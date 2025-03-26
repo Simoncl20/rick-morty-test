2e8651a56f83377a33090b7934355a42a703a42f
Simoncl20 <simoncalderonl2010@gmail.com>
2025-03-25 23:54:23 -0500

    Enhance NavBar styling with shadow and increased text size

--- app/(components)/NavBar.tsx
+++ app/(components)/NavBar.tsx
@@ -4,11 +4,11 @@ import Link from "next/link"
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between py-2 font-bold px-50">
+    <nav className="flex justify-between py-2 font-bold shadow-md px-50">
       <Link href="/">
       <Image src="/rick_morty_logo.svg" width={80} height={50} alt="Rick &Morty Logo"/>
       </Link>
-      <div className="flex items-center gap-6 ">
+      <div className="flex items-center gap-6 text-lg">
         <Link href="/" className="transition-colors duration-300 hover:text-green-400 hover:scale-105">
           Characters
         </Link>

2bf3089e4acf424c041e0e2227c5a7073e020a84
Simoncl20 <simoncalderonl2010@gmail.com>
2025-03-26 04:03:51 -0500

    Enhance error handling in getCharacters function to return user-friendly messages

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CharacterQuery, CharactersResponse } from "./definitions";
 
 export async function getCharacters(
@@ -6,15 +6,29 @@ export async function getCharacters(
     page: string,
 ) {
     try {
-        const response : CharactersResponse = await axios.get('https://rickandmortyapi.com/api/character', {
+        const response = await axios.get('https://rickandmortyapi.com/api/character', {
             params: {
                 page,
                 ...query 
             }
         });
-        return response;
+        return response.data as CharactersResponse;
     } catch (error) {
+        const axiosError = error as AxiosError;
+        
+        if (axiosError.response?.status === 404) {
+            return {
+                error: true,
+                status: 404,
+                message: 'No se encontraron personajes con esos criterios'
+            };
+        }
+        
         console.error('Fetch Error', error);
-        throw new Error('Failed to fetch Characters');
+        return {
+            error: true,
+            status: axiosError.response?.status || 500,
+            message: 'Error al buscar personajes'
+        };
     }
 }
